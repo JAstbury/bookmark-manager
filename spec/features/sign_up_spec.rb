eb7f3c454ed2766fd380777706f0b7b8e3601837
Zak Richardson <zakrc.richardson@gmail.com>
2016-07-28 17:37:14 +0100

    Add email validation

--- spec/features/sign_up_spec.rb
+++ spec/features/sign_up_spec.rb
@@ -6,4 +6,22 @@ feature 'Signing up' do
     expect(page).to have_content('Hello, test@test.com')
     expect(User.first.email).to eq 'test@test.com'
   end
+
+  scenario 'I can\'t sign up with out a email address' do
+    expect{ sign_up(email: nil) }.not_to change(User, :count)
+  end
+
+  scenario 'I can\'t sign up with an invalid email address' do
+    expect{ sign_up(email: "invalid@email" ) }.not_to change(User, :count)
+  end
+
+  def sign_up(email:                  'test@test.com',
+              password:               'password',
+              password_confirmation:  'password')
+    visit '/users/new'
+    fill_in :email,    with: email
+    fill_in :password, with: password
+    fill_in :password_confirmation, with: password_confirmation
+    click_button 'Sign up'
+  end
 end
