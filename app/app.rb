69d500f05b162c7caff79ab917174d83a8c7432d
tchungnz <tchung.nz@gmail.com>
2016-07-28 09:54:05 +0100

    End of day-3

--- app/app.rb
+++ app/app.rb
@@ -1,8 +1,7 @@
-require 'sinatra/base'
-require_relative 'data_mapper_setup'
-
 ENV['RACK_ENV'] = 'development'
 
+require 'sinatra/base'
+require_relative 'data_mapper_setup'
 
 class BookmarkManager < Sinatra::Base
   get '/links' do
