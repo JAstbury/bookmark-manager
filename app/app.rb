8e9f2c154f19a45fbf3f2ee3d011d17b1723d718
tchungnz <tchung.nz@gmail.com>
2016-07-29 11:16:59 +0100

    Implemented sinatra flash for password not correct

--- app/app.rb
+++ app/app.rb
@@ -1,6 +1,7 @@
 ENV['RACK_ENV'] ||= 'development'
 
 require 'sinatra/base'
+require 'sinatra/flash'
 require_relative 'data_mapper_setup'
 # require './app/models/user.rb'
 
@@ -9,15 +10,22 @@ class BookmarkManager < Sinatra::Base
 
 enable :sessions
 set :session_secret, 'super secret'
+register Sinatra::Flash
 
     get '/user/new' do
+      @user = User.new
       erb :'user/new'
     end
 
     post '/users' do
-      user = User.create(email: params[:email], password: params[:password], password_confirmation: params[:password_confirmation])
-      session[:user_id] = user.id
-      redirect to '/links'
+      @user = User.new(email: params[:email], password: params[:password], password_confirmation: params[:password_confirmation])
+      if @user.save
+        session[:user_id] = @user.id
+        redirect to '/links'
+      else
+        flash.now[:notice] = 'Password and confirmation password do not match'
+        erb :'user/new'
+      end
     end
 
     get '/links' do
--- spec/features/user_management_spec.rb
+++ spec/features/user_management_spec.rb
@@ -5,7 +5,9 @@ require './app/models/user.rb'
 feature 'User sign in' do
   scenario 'requires a matching confirmation password' do
     expect{ sign_up(password_confirmation: 'wrong')}.not_to change(User, :count)
-
+    expect(current_path).to eq '/users'
+    expect(page).to have_content 'Password and confirmation password do not match'
+    expect(page).to have_selector("input[value='TC@makers.academy.com']")
   end
 
 end
